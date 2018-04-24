512cbfbb0a07a1f84b1bd29b33f0f0e7dfa7ce9c
Feng, Chao <fiona.feng@pearson.com>
2018-04-24 10:08:05 -0400

    Final Update

---
+++ site.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+$(document).ready(function () {
+
+
+	$(".title").hover(function(){
+		
+		
+		$(".title a").animate({fontSize:'3em'},"slow");
+		
+		$(".title a").text("GO AND ORDER");
+		
+		$(".title a").animate({fontSize:'2.5em'},"slow");
+
+		
+	});
+	
+	
+	
+	
+});
